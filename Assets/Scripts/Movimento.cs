90b16d791711e1693d6e6933a7dfa483c1b06d16
Vinícius <viniciustorch62@gmail.com>
2025-05-15 09:20:04 -0300

    Segundo commit

--- Assets/Scripts/Movimento.cs
+++ Assets/Scripts/Movimento.cs
@@ -1,23 +1,39 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Threading;
 using UnityEngine;
 
 public class Movimento : MonoBehaviour
 {
-    public float velocidade = 10f;
+    public float magnitude = 0.3f;
+    private bool emX;
+    private bool emY;
+    private bool emZ;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        emX = (Random.value > 0.6f);
+        emY = (Random.value > 0.6f);
+        emZ = (Random.value > 0.6f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.Translate(Vector3.right * velocidade * Time.deltaTime * Mathf.Cos(Time.timeSinceLevelLoad), Space.World);
+        // Movimento em X
+        if (emX)
+        {
+            gameObject.transform.Translate(Vector3.right * Mathf.Cos(Time.timeSinceLevelLoad) * magnitude, Space.World);
+        }
+
+        // Movimento em Y
+        if (emY) 
+        {
+            gameObject.transform.Translate(Vector3.back * Mathf.Cos(Time.timeSinceLevelLoad) * magnitude, Space.World);
+        }
+
+        // Movimento em Z
+        if (emZ)
+        {
+            gameObject.transform.Translate(Vector3.up * Mathf.Cos(Time.timeSinceLevelLoad) * magnitude, Space.World);
+        }
     }
 }
